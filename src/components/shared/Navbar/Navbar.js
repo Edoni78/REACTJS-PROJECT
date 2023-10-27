770d1c073f7ee9c5304744e513aae185423f7a8c
Edon Aliu <edonaliu8@gmail.com>
2023-10-27 10:40:07 +0200

    mob-nav

--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -1,60 +1,73 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import {AiFillCloseCircle} from 'react-icons/ai';
 
 
 const Navbar = () => {
+  const [openNav, setOpenNav] = useState(false);
 
-    const toggleNav = () => {
-        const hamburger = document.querySelector('.hamburger-menu');
+  const handleNav = () => {
+    setOpenNav(!openNav);
+  };
 
-        hamburger.addEventListener("click", () => {
-   
-        const navLink = document.getElementById("nav-links");
-
-        if(navLink.style.display === "none"){
-        navLink.style.display = "flex";
-        }else{
-        navLink.style.display = "none";
-        }
-})
-    }
 
   return (
-   <>
+    <>
       <div className="navbar">
-        <Link to='/' className="nav-logo">
-            <div className="logo"></div>
-            <h1>LANGUAGEKOS</h1>
+        <Link to="/" className="nav-logo">
+          <div className="logo"></div>
+          <h1>LANGUAGEKOS</h1>
         </Link>
 
-        <div id='nav-links'  className="nav-links">
-            <Link to='/'>Home</Link>
-            <Link to='/'>Company</Link>
-            <Link to='/'>Courses</Link>
-            <Link to='/'>Teachers</Link>
-            <Link to='/'>Shop</Link>
-            <Link to='/'>Blog</Link>
-            <Link to='/'>Contact Us</Link>
+        <div id="nav-links" className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/">Company</Link>
+          <Link to="/">Courses</Link>
+          <Link to="/">Teachers</Link>
+          <Link to="/">Shop</Link>
+          <Link to="/">Blog</Link>
+          <Link to="/">Contact Us</Link>
         </div>
 
         <div className="nav-right">
-            <div className="nav-icon"></div>
-            <Link to='/' className="login">Log In</Link>
-            <Link to='/' className="register">Register</Link>
+          <div className="nav-icon"></div>
+          <Link to="/" className="login">
+            Log In
+          </Link>
+          <Link to="/" className="register">
+            Register
+          </Link>
         </div>
-        <div className="hamburger-menu" onClick={toggleNav}>
-            <div className="line"></div>
-            <div className="line"></div>
-            <div className="line"></div>
+        <div className="hamburger-menu" onClick={handleNav}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
         </div>
-    </div>
-   </>
 
+        {openNav && (
 
-  )
-}
+            <div className="mob-nav">
+            {/* <AiFillCloseCircle 
+            className="xxx" 
+            onClick={() => setOpenNav(false)}
+            setOpenNav={setOpenNav}
+            /> */}
+               <div id="nav-links2">
+            <Link to="/">Home</Link>
+            <Link to="/">Company</Link>
+            <Link to="/">Courses</Link>
+            <Link to="/">Teachers</Link>
+            <Link to="/">Shop</Link>
+            <Link to="/">Blog</Link>
+            <Link to="/">Contact Us</Link>
+          </div>
+            </div>
+       
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
