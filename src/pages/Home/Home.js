5acfbf13fbc0f72d9d5352a02c90857eb35cabef
Edon Aliu <edonaliu8@gmail.com>
2023-10-29 00:11:55 +0200

    Reallyfinal

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import './Home.css'
 import Navbar from '../../components/shared/Navbar/Navbar'
 import Banner from '../../components/pages/Home/Banner/Banner'
 import Cards from '../../components/pages/Home/Cards/Cards'
@@ -8,11 +9,36 @@ import SectionThree from '../../components/pages/Home/SectionThree/SectionThree'
 import SectionFour from '../../components/pages/Home/SectionFour/SectionFour'
 import SectionFive from '../../components/pages/Home/SectionFive/SectionFive'
 import Footer from '../../components/shared/Footer/Footer'
+import GridLoader from "react-spinners/GridLoader";
 
 
 const Home = () => {
+  const [loading,setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  },[])
+
   return (
-    <div>
+    <div className='home'>
+      {
+        loading ?
+      <div className='loading-icon'>
+  <GridLoader
+        color={'#D94645'}
+        loading={loading}
+        size={30}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      </div>
+
+        :
+
+        <>
         <Navbar/>
         <Banner/>
         <Cards/>
@@ -22,6 +48,17 @@ const Home = () => {
         <SectionFour/>
         <SectionFive/>
         <Footer/>
+        </>
+      }
+        {/* <Navbar/>
+        <Banner/>
+        <Cards/>
+        <SectionOne/>
+        <SectionTwo/>
+        <SectionThree/>
+        <SectionFour/>
+        <SectionFive/>
+        <Footer/> */}
     </div>
   )
 }
