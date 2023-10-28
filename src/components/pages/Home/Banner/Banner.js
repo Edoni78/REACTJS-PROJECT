2c54b1d0a03d3fb401774921c8a4e32898710418
Edon Aliu <edonaliu8@gmail.com>
2023-10-28 16:17:48 +0200

    error solved

--- src/components/pages/Home/Banner/Banner.js
+++ src/components/pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Banner.css'
+import { Link } from 'react-router-dom'
 
 const Banner = () => {
   return (
@@ -9,8 +10,8 @@ const Banner = () => {
             <h1>Learn every language you want in 10 minutes a day</h1>
             <p>Pick anything from our 100+ courses and start learning your favorite language now</p>
             <div className="banner-buttons">
-                <a href="" className="getStarted">Get Started </a>
-                <a href="">Learn More <span className="arrow-icon"></span></a>
+                <Link href="" className="getStarted">Get Started </Link>
+                <Link href="">Learn More <span className="arrow-icon"></span></Link>
             </div>
         </div>
 
